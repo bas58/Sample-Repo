33bf67bbece816328fadeb990eb33d44d93ac236
bas58 <b.stuivenberg@kpnmail.nl>
2020-10-05 18:58:15 +0200

    Removed some code

--- script.py
+++ script.py
@@ -1,16 +1,6 @@
-import sys
-import math
-from os import rename
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hallo, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://basstuiv@gmail.com")
 print(r.status_code)
+print(r.ok)
